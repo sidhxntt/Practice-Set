9800307f7da6ee5496220c4fb1108ac078329672
Siddhantg2002 <siddhantg2002@gmail.com>
2024-04-27 00:44:57 +0530

    Sample blog using React

---
+++ REACT/sample-blog/src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/sample-blog/src/Components/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    const title ="Homepage"
+    return (
+      <div className="home">
+        <h2>{title}</h2>
+      </div>
+    );
+  }
+   
+  export default Home;
