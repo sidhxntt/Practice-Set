b08804246d0ddb41248d602e8c49d65750b6ba42
Sid <siddhantg2002@gmail.com>
2025-08-22 11:31:37 +0530

    feat(LEETCODE): implement new functionality

--- LEETCODE/DSA & Algorithms/Basics/ADTs/strings.py
+++ LEETCODE/DSA & Algorithms/Basics/ADTs/strings.py
@@ -1,10 +1,17 @@
 from utils.displayer import logger, Displayer
 from arrays import Arrays
-class Strings(Arrays):
+
+class Strings(Displayer):
 
     def display(self, arr): 
         logger.info(f"Current array: {arr}")    # display the array O(1)
 
+    def Find_char(self, arr:str, val:str): 
+        if val in arr:
+            i = arr.find(val)
+            logger.info(f"Value {val} found at index {i}!")    
+        else:
+            logger.warning(f"Value {val} not found!")
    
 
 # Usage
@@ -13,24 +20,11 @@ if __name__ == "__main__":
 
     str1 = "Siddhant Gupta"
     str2 = str1 # strings are immutable unlike lists in py
-    str3 = "Priyanka Mishra"
-    # str4 = "Sanskar Mohanty"
 
     operations = [
         ("Original array", a.display, str1),
-        ("Add element 99", a.append_ele, str1, 99),
-        ("Insert 23 at index 3", a.insert_ele, str1, 23, 3),
-        ("Delete element 5", a.delete_ele, str1, 5),
-        ("Delete element at index 2", a.delete_ele_from_index, str1, 2),
-        ("Linear search 99", a.linear_search, str1, 99),
-        ("Binary search 7", a.binary_search, str1, 7),
-        ("Is sorted or not", a.is_sorted_or_not, str2),
-        ("Reversed", a.reverse, str2),
-        # ("Merged Arrays", a.merge_arrays, str3, str4),
-        # ("Merged Arrays at middle", a.merge_arrays_at_middle, str3, str4),
-        # ("Union of Arrays", a.union, str3, str4),
-        # ("Intersection of Arrays", a.intersection, str3, str4),
-        # ("Difference of Arrays", a.difference, str3, str4),
+        ("Index of the char G", a.Find_char, str1, "G"),
+      
     ]
 
     Displayer.execute(operations)
