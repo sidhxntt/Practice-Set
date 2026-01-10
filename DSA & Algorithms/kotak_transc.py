168b6976f502ab76eeb894622ac1f9e47412f331
Sid. <siddhantg2002@gmail.com>
2026-01-10 12:13:31 +0530

    feat(DSA & Algorithms): add kotak_transc.py

---
+++ DSA & Algorithms/kotak_transc.py
@@ -0,0 +1,7 @@
+cost = [142, 500,386, 2280, 400, 818, 206, 8681 , 5700, 5000, 2500 ]
+# 8681 rent, 5000 mom, 2500 hathaway, 5700 potien, 2280 rohan
+sum =0
+for i in range(len(cost)):
+    sum = sum + cost[i]
+print("Total cost is:", sum)
+    
